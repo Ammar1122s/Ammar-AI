ce21a87d9d061da3ced679cc9e9f7a0f6f2dc126
Ammar1122s <you@example.com>
2023-03-16 16:23:05 +0500

    16-3-23

---
+++ new.py
@@ -0,0 +1,62 @@
+
+grpah = { "A":["B","C"],
+         "B":["F","A"],
+         "C":["F","D","E","A"],
+         "D":["C"],
+         "E":["G","C"],
+         "F":["B","C"],
+         "G":["E"]}
+
+
+node = input("Enter the Node: ")
+
+node = node.upper()
+
+open =[]
+close = []
+
+check = True
+open.append(("/","A"))
+
+print(open)
+
+while(check):
+    if len(open) == 0:
+        print("Target not Found")
+        break
+    n = open.pop()
+    print(n[1])
+    if n[1] == node:
+        print("Target Hits...")
+        check= False
+
+    children = grpah[n[1]]
+
+    print(children)
+
+    for child in children[::-1]:
+        if not ((n[1],child) in close):
+            open.append((n[1],child))
+    # print(open)
+    close.append(n)
+    # print(close)
+
+
+    
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
